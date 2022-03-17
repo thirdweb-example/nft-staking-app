4dfaae9e0e8660191fd0fe45f2982a4b63e4e70a
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-03-17 19:22:19 +0100

    Add disconnect wallet button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
-import { useAddress, useMetamask } from '@thirdweb-dev/react';
+import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   const address = useAddress();
   const connectWithMetamask = useMetamask();
+  const disconnectWallet = useDisconnect();
   return (
     <div>
       {address ? (
-        <p>Your address is {address}</p>
+        <>
+          <button onClick={disconnectWallet}>Disconnect Wallet</button>
+          <p>Your address: {address}</p>
+        </>
       ) : (
         <button onClick={connectWithMetamask}>Connect with Metamask</button>
       )}
