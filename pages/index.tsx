fdf2dcdaae9c700f5e88e4631d681742fcc31535
Ashish Mittal <ashish.mittal731@gmail.com>
2022-08-25 02:10:57 +0530

    Fixed DOM and CSS bit

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,19 +9,18 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       {/* Top Section */}
       <h1 className={styles.h1}>thirdweb Deploy - Custom Staking Contract</h1>
-
-      <div
-        className={styles.nftBoxGrid}
-        role="button"
-        onClick={() => router.push(`/mint`)}
-      >
-        {/* Mint a new NFT */}
-        <div className={styles.optionSelectBox}>
-          <img src={`/icons/drop.webp`} alt="drop" />
-          <h2 className={styles.selectBoxTitle}>Mint a new NFT</h2>
-          <p className={styles.selectBoxDescription}>
-            Use the NFT Drop Contract to claim an NFT from the collection.
-          </p>
+      <div className={styles.nftBoxGrid}>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/mint`)}
+        >
+          {/* Mint a new NFT */}
+            <img src={`/icons/drop.webp`} alt="drop" />
+            <h2 className={styles.selectBoxTitle}>Mint a new NFT</h2>
+            <p className={styles.selectBoxDescription}>
+              Use the NFT Drop Contract to claim an NFT from the collection.
+            </p>
         </div>
 
         <div
@@ -29,6 +28,7 @@ const Home: NextPage = () => {
           role="button"
           onClick={() => router.push(`/stake`)}
         >
+          {/* Staking an NFT */}
           <img src={`/icons/token.webp`} alt="drop" />
           <h2 className={styles.selectBoxTitle}>Stake Your NFTs</h2>
           <p className={styles.selectBoxDescription}>
