9c0dda703827172363013508a64b044cd564b889
shendel <im_shendel@inbox.ru>
2023-01-12 23:30:43 +0300

    static (github pages work)

--- prepare-static.js
+++ prepare-static.js
@@ -1,22 +1,33 @@
 const glob = require('glob')
 const { readFileSync, writeFileSync } = require('fs')
+const fsExtra = require('fs-extra')
 
 const root_folder = './out'
 const _extenstions = ['*.html','*.js','*.css']
 
-_extenstions.forEach((ext) => {
-  glob(root_folder + '/**/' + ext, (err, files) => {
-    if (err) {
-      console.log('Error', err)
-    } else {
-      files.forEach((file) => {
-        const content = readFileSync(file, 'utf8');
+console.log('Prepare engine')
+fsExtra.move(root_folder + '/_next/', root_folder + '/engine/', err => {
+  if(err) return console.error(err);
+  console.log('Make engine success!');
 
-        let newContent = content.replaceAll('/NFTFARMBUILDURL', '.');
+  console.log('>>> Fix paths')
+  _extenstions.forEach((ext) => {
+    glob(root_folder + '/**/' + ext, (err, files) => {
+      if (err) {
+        console.log('Error', err)
+      } else {
+        files.forEach((file) => {
+          const content = readFileSync(file, 'utf8');
 
-        console.log(file);
-        writeFileSync(file, newContent);
-      })
-    }
+          let newContent = content.replaceAll('/NFTFARMBUILDURL', '.');
+          newContent = newContent.replaceAll('/_next/','/engine/');
+
+          console.log(file);
+          writeFileSync(file, newContent);
+        })
+      }
+    })
   })
 })
+
+
