1807f58c40010bf9b02554ad025c732492e72181
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-03-13 17:43:41 +0530

    small update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-// This is the chainId your dApp will work on.
+// This is the chain your dApp will work on.
 const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
