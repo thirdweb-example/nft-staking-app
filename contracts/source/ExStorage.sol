134d5da207aa14630c15d52555d50cd81c5ceb93
shendel <im_shendel@inbox.ru>
2023-03-19 00:44:39 +0300

    ExStorage - optimize

--- contracts/source/ExStorage.sol
+++ contracts/source/ExStorage.sol
@@ -7,18 +7,7 @@ interface IStorage {
         string info;
     }
 
-    struct KeyData {
-        string key;
-        Data data;
-    }
-
     function getData(string memory _key) external view returns(Data memory);
-    function allKeys() external view returns(string[] memory);
-    function allKeysData() external view returns(Data[] memory);
-    function setKeyData(string memory _key, Data memory _data) external;
-    function setKeysData(KeyData[] memory _keysData) external;
-    function clearKeyData(string memory _key) external;
-    function clearKeysData(string[] memory _keys) external;
 }
 
 contract ExStorage {
@@ -35,49 +24,35 @@ contract ExStorage {
         _;
     }
 
+    modifier onlyScopeOwner(string memory _scopeKey) {
+        IStorage.Data memory _scopeInfo = mainStorage.getData(_scopeKey);
+        if (_scopeInfo.owner != address(0)) {
+            require(msg.sender == _scopeInfo.owner, 'FORBIDDEN');
+        } else {
+            require(msg.sender == address(0), 'NOT CONFIGURED IN MAIN STORAGE');
+        }
+        _;
+    }
+
     mapping (string => mapping (string => string)) public scopeData;
     mapping (string => string[]) public scopeDataKeys;
     mapping (string => mapping (string => bool)) public scopeDataKeyExists;
 
 
-
-    
     constructor(address _mainStorage) {
         mainStorage = IStorage(_mainStorage);
-    }
-
-    function _getScopeOwner(string memory _scopeKey) private view notEmpty(_scopeKey) returns (address) {
-        IStorage.Data memory _scopeInfo = mainStorage.getData(_scopeKey);
-        
-        return _scopeInfo.owner;
-    }
-
-    function checkScopeOwner(string memory scopeKey)
-        public
-        view
-        notEmpty(scopeKey)
-        returns (bool)
-    {
-        address scopeOwner = _getScopeOwner(scopeKey);
-        if (scopeOwner != address(0)) {
-            require(msg.sender == scopeOwner, 'FORBIDDEN');
-        } else {
-            require(msg.sender == address(0), 'NOT CONFIGURED IN MAIN STORAGE');
-        }
-        return true;
     }
 
     function clearScopeData(string memory _scopeKey)
         public
         notEmpty(_scopeKey)
+        onlyScopeOwner(_scopeKey)
     {
-        if (checkScopeOwner(_scopeKey)) {
-            for (uint256 kIndex = 0; kIndex < scopeDataKeys[_scopeKey].length; kIndex++) {
-                scopeDataKeyExists[_scopeKey][scopeDataKeys[_scopeKey][kIndex]] = false;
-                scopeData[_scopeKey][scopeDataKeys[_scopeKey][kIndex]] = "";
-            }
-            delete scopeDataKeys[_scopeKey];
+        for (uint256 kIndex = 0; kIndex < scopeDataKeys[_scopeKey].length; kIndex++) {
+            scopeDataKeyExists[_scopeKey][scopeDataKeys[_scopeKey][kIndex]] = false;
+            scopeData[_scopeKey][scopeDataKeys[_scopeKey][kIndex]] = "";
         }
+        delete scopeDataKeys[_scopeKey];
     }
 
     function getScopeData(string memory _scopeKey)
@@ -119,13 +94,8 @@ contract ExStorage {
         notEmpty(_scopeKey)
         notEmpty(_key)
         notEmpty(_data)
+        onlyScopeOwner(_scopeKey)
     {
-        address scopeOwner = _getScopeOwner(_scopeKey);
-        if (scopeOwner != address(0)) {
-            require(msg.sender == scopeOwner, 'FORBIDDEN');
-        } else {
-            require(msg.sender != address(0), 'NOT CONFIGURED IN MAIN STORAGE');
-        }
         scopeData[_scopeKey][_key] = _data;
         if (!scopeDataKeyExists[_scopeKey][_key]) {
             scopeDataKeyExists[_scopeKey][_key] = true;
