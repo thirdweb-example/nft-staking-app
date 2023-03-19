356d24e83d10824b6be48acf00ec8ea10cd0ea2e
Weston Nelson <dogooddowell@protonmail.com>
2023-03-19 04:10:53 +0000

    Update Contracts

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,10 @@
+import { CONTRACT_ADDRESSES } from "@thirdweb-dev/sdk";
+
 export const nftDropContractAddress =
   "0x00e3aA03e47c32397a94509E50B0558988C0D04E";
 export const tokenContractAddress =
   "0xc96f4F893286137aC17e07Ae7F217fFca5db3AB6";
 export const stakingContractAddress =
   "0xead23B8C960C222E51197b831D2eB18FBB870520";
+
+export default CONTRACT_ADDRESSES;
