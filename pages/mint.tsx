ab388594d0811f9d57802267ee5ff4a68085569f
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-04-04 22:30:50 +0530

    feat: update template (#32)
    * feat: update template
    
    * fix: build

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -18,8 +18,7 @@ const Mint: NextPage = () => {
       <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
 
       <Web3Button
-        colorMode="dark"
-        accentColor="#5204BF"
+        theme="dark"
         contractAddress={nftDropContractAddress}
         action={(contract) => contract.erc721.claim(1)}
         onSuccess={() => {
